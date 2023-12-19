70cfcb7ec831da4d6e8cba356998e8c5e8efb417
kssampson <kylesampsonmusic@gmail.com>
2023-12-18 19:58:38 -0800

    implemented get request for available guitars

--- guitar_checkout/database/controllers/guitars.js
+++ guitar_checkout/database/controllers/guitars.js
@@ -2,7 +2,18 @@ const Guitars = require('../models/guitars.js');
 
 module.exports = {
   getAll: (req, res) => {
-
+    Guitars.find(
+      {available: true},
+      {builderName: 1, specs: 1, description: 1, _id: 0}
+    )
+    .then((results) => {
+      console.log('results: ', results)
+      res.json(results)
+    })
+    .catch((err) => {
+      console.log('err getting available guitars: ', err)
+      res.sendStatus(500)
+    })
   },
   createGuitar: async (req, res) => {
     Guitars.create(req.body)
